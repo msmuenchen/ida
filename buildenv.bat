94da16dc1751dcb383121b5c37484adb4f277524
Marco Schuster <marco@m-s-d.eu>
2013-01-23 12:56:47 +0100

    initial commit

---
+++ buildenv.bat
@@ -0,0 +1,1 @@
+set PATH=%PATH%;c:\php-sdk\php53dev\vc9\x86\php-5.3.6\Release_TS\;c:\Program Files (x86)\nasm\;C:\Users\Marco\AppData\Local\GitHub\PortableGit_93e8418133eb85e81a81e5e19c272776524496c6\cmd\;
